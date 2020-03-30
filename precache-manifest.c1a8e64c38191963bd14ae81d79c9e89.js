01bd6e8608f77c95c5751027ea697b96b471352b
IP777 <ivanov_piter@ukr.net>
2020-03-30 23:41:49 +0300

    Updates

---
+++ precache-manifest.c1a8e64c38191963bd14ae81d79c9e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67624e235b40ffd8dbec5233bda6b947",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "281ef14d28b63fe8ae52",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.a93d517c.chunk.css"
+  },
+  {
+    "revision": "c1120fed9c084dde6005",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.873111cf.chunk.js"
+  },
+  {
+    "revision": "dfd9de20f4ff60fc8200b143aaf0a20e",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.873111cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281ef14d28b63fe8ae52",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.b4696361.chunk.js"
+  },
+  {
+    "revision": "cc5d00c14a25a053fbb7",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.e35af0b9.js"
+  }
+]);
