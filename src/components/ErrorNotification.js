fefe5ad08e38292617f02a7cf6c1f3c543691a4b
IP777 <ivanov_piter@ukr.net>
2020-03-31 15:26:08 +0300

    edits 4

--- src/components/ErrorNotification.js
+++ src/components/ErrorNotification.js
@@ -4,7 +4,9 @@ const style = {
 	color: "#ff0000",
 	display: "block",
 	fontSize: 14,
-	fontWeight: 300
+	fontWeight: 300,
+	margin: 0,
+	marginBottom: 5
 };
 
 const ErrorNotificatrion = ({ errorType }) => (
